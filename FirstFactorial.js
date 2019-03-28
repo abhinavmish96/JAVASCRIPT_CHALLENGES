1fc021b7c1cbdf86e3d94397542c3ae3cc85f61d
Abhinav Mishra <abhinavmish96@gmail.com>
2019-03-28 13:25:02 +0530

    <Day 1>

---
+++ FirstFactorial.js
@@ -0,0 +1,27 @@
+/* The function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer */
+
+function FirstFactorial(num){
+    
+    // to store factorial; assign 1 otherwise you might see bizzare result such as NaN
+    let fact = 1;
+
+    // For value 0 and 1 the factorial is always 1
+    if(num === 0 || num ===1 ) return 1;
+    
+    // For other values calculate the factorial
+    else{
+        // loop to caluculate factorial from 1 to num
+        for(let i =1; i<=num; i++ ){
+            fact = fact*i;
+        }
+        // return factorial
+        return fact;
+    }
+
+}
+
+// call the function for calculating the factorial and you can give your favourate number as argument
+let result = FirstFactorial(4);
+
+//Display the result 
+console.log(result);
---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+# Hello World!
+
+##This is a collection of JavaScript programs/challenges with line by line explanation for anyone who is learning to code in JavaScript [JS].
+
+## Just follow the flow to get the best out of these challenges, try the below mentioned challenges and become an expert in JS:
+1. Calculate factorial for a given number
+2. 
+
+### Suggestion and feedback are always welcome, feel free to fork and make changes!!
+
+# Follow me @abhinavmish96 on twitter to clarify any doubts or just drop a Hi!!!
+
+#Start today! Happy Coding!!
