e23932ef873a8a80170f39fbd5002c7d4d9e7df5
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-03 14:17:46 +0530

    <Program 9>

--- 9_AlphabetSoup_S.js
+++ 9_AlphabetSoup_S.js
@@ -1,1 +1,34 @@
 /* function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string. */
+
+
+function AlphabetSoup(str) { 
+
+    // code goes here
+    let x = '';
+    str = str.trim().toLowerCase().split('');
+    for(let j = 0; j < str.length ; j++){
+    for(let i = j + 1; i < str.length ; i++){
+    if(str[j].charCodeAt() > str[i].charCodeAt()){
+        x = str[i];
+        str[i] = str[j];
+        str [j] = x;
+    }
+    }
+    }
+    return str.join(''); 
+           
+  }
+  
+  
+
+
+  function AlphabetSoup(str) { 
+
+    var arrays = str.split("");
+    var reversearrays = arrays.sort();
+    var result = reversearrays.join("");  
+     
+    
+      return result; 
+             
+    }
