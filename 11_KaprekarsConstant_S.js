7984952a18b5a42275c870efaef75e870d5d5783
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-22 18:25:32 +0530

    <Challenge 11>

--- 11_KaprekarsConstant_S.js
+++ 11_KaprekarsConstant_S.js
@@ -4,20 +4,35 @@ function KaprekarsConstant(num){
 
     //a counter to count the number of iteration to reach Kaprekars Constant
     let counter = 0;
+
+    // a loop until 6174 is reached
     while(num !== 6174){
+
+        // converting the number to string
         num = num.toString();
+
+        // calculating the ascending format for the four digit number
         let ascending = num.split('').sort().join('');
+        // calculating the descending format for the four digit number
         let descending = ascending.split('').reverse().join('');
+        // calculating the difference between the ascending and descending to get to next step
+        // also converting the ascending and descending from string to a number
         num = parseInt(descending) - parseInt(ascending);
-        if(num < 1000) num = num * 10;
+        // checking if the number becomes less than four digit then converting it back to four digit
+        if(num < 1000 && num >= 100) num = num * 10;
+        if(num < 100 && num >= 10) num = num * 100;
+        if(num < 10 && num >= 1) num = num * 1000;
+        // if all digit became same then we need to stop the program and return the message
         if(num === 0) return 'All digit same';
+        // if all well counter goes on
         counter++;
     }
 
+    // return the counter
     return counter;
 }
 
-// call the function to perform the comparison and you can give your favourate number as argument; I am using '2111' as an example.
+// call the function to perform the operation and you can give your favourate number as argument; I am using '2111' as an example.
 let check = KaprekarsConstant(2111);
 
 //Display the result 
