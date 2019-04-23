db6a8d03d2eda1248a3a35202c68be5e7361d57e
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-23 23:26:28 +0530

    <Challenge 12>

--- 12_ChessboardTraveling_MS.js
+++ 12_ChessboardTraveling_MS.js
@@ -1,17 +1,27 @@
 /* function ChessboardTraveling(str) read str which will be a string consisting of the location of a space on a standard 8x8 chess board with no pieces on the board along with another space on the chess board. The structure of str will be the following: "(x y)(a b)" where (x y) represents the position you are currently on with x and y ranging from 1 to 8 and (a b) represents some other space on the chess board with a and b also ranging from 1 to 8 where a > x and b > y. Your program should determine how many ways there are of traveling from (x y) on the board to (a b) moving only up and to the right. For example: if str is (1 1)(2 2) then your program should output 2 because there are only two possible ways to travel from space (1 1) on a chessboard to space (2 2) while making only moves up and to the right. */
 
 function ChessboardTraveling(str){
+
+    // to check the correct format of the paramaeter passed
     regEx = /^(dsd)(dsd)$/
     var isFormatted = regEx.test(str);
+
+    // if the format in correct perform computation
     if (isFormatted) {
+        // number of horizontal moves
         var horShift = parseInt(str.charAt(8), 10) - parseInt(str.charAt(3), 10);
+        // number of vertical moves
         var vertShift = parseInt(str.charAt(6), 10) - parseInt(str.charAt(1), 10);
+        // formula to calculate the total number of combination the destination can be achieved
+        // Detailed explaination at the bottom
         return factorial(horShift + vertShift) / (factorial(vertShift) * factorial(horShift));
     } else {
-      return 'error'
+        // else return error
+        return 'error'
     }
 }
 
+// function to calculate factorial using recursion function
 function factorial(num) {
     return num === 0 ? 1 : num * factorial(num - 1);
 }
