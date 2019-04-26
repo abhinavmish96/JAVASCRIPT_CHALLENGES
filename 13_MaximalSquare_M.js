71deff389d53135d42235bd681b007b7c86afefe
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-26 13:43:24 +0530

    <Challenge 13>

--- 13_MaximalSquare_M.js
+++ 13_MaximalSquare_M.js
@@ -11,44 +11,46 @@ function MaximalSquare(strArr){
     // using map from array prototype for converting each element of the given array into sub array and storing those subarrays into parent array named fullArray
     let fullArray = strArr.map(val => val.split(''));
 
-  let initArray = initialEval(fullArray);
-
-  if (initArray.length === 0) {
-    return 0;
-  }
-  count = 1;
-
-  while (initArray.length) {
-    initArray = initArray.filter(val => {
-      return testOne(fullArray, count + 1, val[0], val[1]);
-    })
-    count++;
-    if (count > 10) break;
-  }
-  return Math.pow(count - 1, 2);
+    let initArray = initialEval(fullArray);
+    if (initArray.length === 0) {
+        return 0;
+    }
+    count = 1;
+    while (initArray.length) {
+        initArray = initArray.filter(val => {
+            return testOne(fullArray, count + 1, val[0], val[1]);
+        })
+        count++;
+        if (count > 10) break;
+    }
+    
+    return Math.pow(count - 1, 2);
 }
 
 function initialEval (arr) {
-  let initArray = [];
-  arr.forEach((row, rowNum) => {
-    row.forEach((val, colNum) => {
-      if (val === '1') {
-        initArray.push([rowNum, colNum]);
-      }
+    
+    let initArray = [];
+    arr.forEach((row, rowNum) => {
+        row.forEach((val, colNum) => {
+            if (val === '1') {
+                initArray.push([rowNum, colNum]);
+            }
+        });
     });
-  });
-  return initArray;
+    
+    return initArray;
 }
 
 function testOne(arr, size, r, c) {
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      if (!arr[r + i] || !arr[r + i][c + j] || arr[r + i][c + j] !== '1') {
-        return false;
-      }
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            if (!arr[r + i] || !arr[r + i][c + j] || arr[r + i][c + j] !== '1') {
+                return false;
+            }
+        }
     }
-  }
-  return true;
+    
+    return true;
 }
 
 // call the function for computing Maximal square and you can give your favourate array of string as argument; I am using "["0111", "1111", "1111", "1111"]" as an example for true.
