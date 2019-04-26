de2d3e41b9f329c656392e3e72a1913764212ba8
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-26 11:28:57 +0530

    <Challenge 13>

--- 13_MaximalSquare_M.js
+++ 13_MaximalSquare_M.js
@@ -1,5 +1,5 @@
 /* function MaximalSquare(strArr) take the strArr parameter being passed which will be a 2D matrix of 0 and 1's, and determine the area of the largest square submatrix that contains all 1's. A square submatrix is one of equal width and height, and your program should return the area of the largest submatrix that contains only 1's. For example: if strArr is ["10100", "10111", "11111", "10010"] then this looks like the following matrix: 
-1 0 0
+1 0 1 0 0
 1 0 1 1 1
 1 1 1 1 1
 1 0 0 1 0 
