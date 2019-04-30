94139b051cffbe091a1cb02301eaa98ced78b9f9
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-30 23:38:28 +0530

    <Challenge 14>

---
+++ 14_PentagonalNumber_M$.js
@@ -0,0 +1,5 @@
+/* function PentagonalNumber(num) read num which will be a positive integer and determine how many dots exist in a pentagonal shape around a center dot on the Nth iteration. For example, in the image below you can see that on the first iteration there is only a single dot, on the second iteration there are 6 dots, on the third there are 16 dots, and on the fourth there are 31 dots.
+
+14_PentagonalNumber_M$.png
+
+Your program should return the number of dots that exist in the whole pentagon on the Nth iteration. */
--- ReadMe.md
+++ ReadMe.md
@@ -36,6 +36,7 @@ Type are into are divided into few categories :
 11. Determine when a specific sequence terminates
 12. Chessboard Traveling - This challenge will require knowledge of basic mathematical combinations.
 13. Searching a matrix for the largest square submatrix
+14. Finding how many dots can fit into a pentagonal shape
 
 If you have problems, create an issue on this repo. Thanks! :)
 
