28ea2a8e863a23982787be388a4bb723d4317a92
Abhinav Mishra <abhinavmish96@gmail.com>
2019-05-18 17:36:23 +0530

    <Challenge 16>

--- 16_ScaleBalancing_MS.js
+++ 16_ScaleBalancing_MS.js
@@ -3,14 +3,43 @@
 There will only ever be one unique solution and the list of available weights will not be empty. It is also possible to add two weights to only one side of the scale to balance it. If it is not possible to balance the scale then your program should return the string not possible. */
 
 function ScaleBalancing(strArr){
-    let scale = [], weights =[];
+    let scale = [];
     strArr[0].split('').forEach((val) => {
         if(!isNaN(parseInt(val))) scale.push(parseInt(val));
     });;
+    scale.sort();
+
+    let weights =[];
     strArr[1].split('').forEach((val) => {
         if(!isNaN(parseInt(val))) a.push(parseInt(val));
     });;
-    return strArr;
+
+    let diff = scale[1] - scale[0];
+
+    //single-weight solution test
+    if(weights.includes(diff)){
+        return diff.toString();
+    }
+
+    //two-weights, one-side test
+    let weight1 = weights.find((val, ind) => {
+        let newWeights = weights.slice(0);newWeights.splice(ind, 1);
+        return newWeights.includes (diff - val)
+    });
+    if (weight1) {
+        return `${weight1},${diff - weight1}`
+    }
+    
+    //two-weights, different sides, test
+    weight1 = weights.find(val => {
+        return weights.includes(diff + val);
+    });
+    if (weight1) {
+        return `${weight1},${diff + weight1}`
+    }
+    
+    //if nothing is returned the return not possible
+    return `not possible`;
 }
 
 // call the function to balance the scale and you can give your favourate string array as argument; I am using '???rrurdr?' as an example.
